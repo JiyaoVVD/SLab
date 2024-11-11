951ce873f4ca75442758ef5f366daf8ee80d5a5d
JiyaoVVD <scutrvvd@gmail.com>
2024-11-11 20:01:19 +0800

    test draw

--- src/main.cpp
+++ src/main.cpp
@@ -34,11 +34,15 @@ SVector3 calcCamPos(SVector3 center, SFloat deg, SFloat dist){
 
 
 int main(){
-//    auto camera = new Camera(SVector3(S_CONST_FLOAT(0.0), S_CONST_FLOAT(0.0), S_CONST_FLOAT(-4.0)), SVector3(0, 0, 0), SVector3(0, 1, 0));
-//    camera->setPerspective(S_CONST_FLOAT(90.0), (SFloat)WIDTH / HEIGHT, S_CONST_FLOAT(0.01), S_CONST_FLOAT(1000.0));
+//    auto camera = new Camera(SVector3(S_CONST_FLOAT(0.0), S_CONST_FLOAT(0.0), S_CONST_FLOAT(-2.0)), SVector3(0, 0, 0), SVector3(0, 1, 0));
+//    camera->setPerspective(S_CONST_FLOAT(60.0), (SFloat)WIDTH / HEIGHT, S_CONST_FLOAT(0.001), S_CONST_FLOAT(1000.0));
 //    // camera->setOrthodox(S_CONST_FLOAT(-0.5), S_CONST_FLOAT(0.5), S_CONST_FLOAT(0.5), S_CONST_FLOAT(-0.5), S_CONST_FLOAT(0.001), S_CONST_FLOAT(100.0));
 //    PrintMatrix<4, 4>(camera->projection());
-//    PrintMatrix<4, 4>(glm::perspectiveLH(glm::radians(90.0f), 1.0f, 0.001f, 1000.0f));
+//    PrintMatrix<4, 4>(glm::perspectiveLH(glm::radians(60.0f), 1.0f, 0.001f, 1000.0f));
+//    auto p = camera->view() * SVector4(0, 0.5, 1.0, 1);
+//    PrintVector<4>(p);
+//    p = camera->projection() * p;
+//    PrintVector<4>(p);
 
     if(!glfwInit()){
         std::cerr << "Failed to initialize GLFW" << std::endl;
@@ -64,12 +68,12 @@ int main(){
     auto deg = S_CONST_FLOAT(0.0);
     SVector3 center(0);
     SVector3 newPos;
-    SFloat dist = S_CONST_FLOAT(4.0);
+    SFloat dist = S_CONST_FLOAT(2.0);
 
     Renderer renderer(WIDTH, HEIGHT, window, Renderer::SLAB_LINE_MODE);
     auto camera = new Camera(SVector3(S_CONST_FLOAT(0.0), S_CONST_FLOAT(0.0), S_CONST_FLOAT(-4.0)), SVector3(0, 0, 0), SVector3(0, 1, 0));
-//    camera->setPerspective(S_CONST_FLOAT(90.0), (SFloat)WIDTH / HEIGHT, S_CONST_FLOAT(0.01), S_CONST_FLOAT(1000.0));
-    camera->setOrthodox(S_CONST_FLOAT(-0.5), S_CONST_FLOAT(0.5), S_CONST_FLOAT(0.5), S_CONST_FLOAT(-0.5), S_CONST_FLOAT(0.001), S_CONST_FLOAT(100.0));
+    camera->setPerspective(S_CONST_FLOAT(90.0), (SFloat)WIDTH / HEIGHT, S_CONST_FLOAT(0.01), S_CONST_FLOAT(1000.0));
+    // camera->setOrthodox(S_CONST_FLOAT(-0.5), S_CONST_FLOAT(0.5), S_CONST_FLOAT(0.5), S_CONST_FLOAT(-0.5), S_CONST_FLOAT(0.001), S_CONST_FLOAT(100.0));
     renderer.setCamera(camera);
     while(!glfwWindowShouldClose(window)){
         processInput(window);
